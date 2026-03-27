7c4e07b9e05d247c61b996d8f0cbb974026c05f7
Marco <mipeppe@outlook.es>
2026-03-26 21:46:00 -0500

    Se modificó programa.java agregando nuevos mensajes.

--- folder/programa.java
+++ folder/programa.java
@@ -2,8 +2,8 @@ public class Programa {
     String mensaje = "Hola mundo";
     public static void main(String[] args) {
         System.out.println(mensaje);
-
-        // cambios hecho en el pograma
+         
+        // cambios hecho en el pograma..
         System.out.println("Retoques realizados");
     }
 }
